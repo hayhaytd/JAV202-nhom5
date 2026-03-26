c6173474d57762732be7cecad51445b533133755
Minh Khoi <vohoangminhkhoi2412@gmail.com>
2026-03-26 21:01:42 +0700

    bai 1+2 lab 4

---
+++ Lab3/polycafe/src/main/java/util/AuthUtil.java
@@ -0,0 +1,28 @@
+package utils;
+
+import jakarta.servlet.http.HttpServletRequest;
+import entity.User;
+
+public class AuthUtil {
+
+    public static void setUser(HttpServletRequest request, User user) {
+        request.getSession().setAttribute("user", user);
+    }
+
+    public static User getUser(HttpServletRequest request) {
+        return (User) request.getSession().getAttribute("user");
+    }
+
+    public static boolean isAuthenticated(HttpServletRequest request) {
+        return getUser(request) != null;
+    }
+
+    public static boolean isManager(HttpServletRequest request) {
+        User user = getUser(request);
+        return user != null && user.getRole() == 1; // 1 = manager
+    }
+
+    public static void clear(HttpServletRequest request) {
+        request.getSession().removeAttribute("user");
+    }
+}
